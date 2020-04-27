277b5a04014ab523d66d59aa7351ff0893fbe105
JAIDEEP_C <51963564+Jaideep-C@users.noreply.github.com>
2020-04-27 16:21:50 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/easy_solve/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.easy_solve
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+// import 'package:mi_card/Es.dart';
+void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home:Text("Hi PPl"),
+      );
+  }
+}
