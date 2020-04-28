498e5a39b8c781c6c2043904c5264dcebf79baba
JAIDEEP_C <51963564+Jaideep-C@users.noreply.github.com>
2020-04-28 14:41:58 +0530

    LoginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
+import 'package:easy_solve/src/welcomePage.dart';
 import 'package:flutter/material.dart';
-// import 'package:mi_card/Es.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
-      home:Text("Hi PPl"),
-      );
+      theme: ThemeData(
+         primarySwatch: Colors.blue,
+         textTheme:GoogleFonts.latoTextTheme(textTheme).copyWith(
+           body1: GoogleFonts.montserrat(textStyle: textTheme.body1),
+         ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: WelcomePage(),
+    );
   }
 }
+
---
+++ lib/src/Widget/bezierContainer.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'customClipper.dart';
+
+class BezierContainer extends StatelessWidget {
+  const BezierContainer({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Transform.rotate(
+        angle: -pi / 7.1, 
+        child: ClipPath(
+        clipper: ClipPainter(),
+        child: Container(
+          height: MediaQuery.of(context).size.height *.5,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                // end: Alignment.bottomRight,
+                colors: [Color(0xff3a7bd5),Color(0xff00d2ff)]
+              )
+            ),
+        ),
+      ),
+      )
+    );
+  }
+}
