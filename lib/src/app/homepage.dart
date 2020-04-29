4316e6c74e5da4765915da0a3f54f9e2ab758f9f
JAIDEEP_C <51963564+Jaideep-C@users.noreply.github.com>
2020-04-29 16:26:37 +0530

    HomePage Update

--- lib/src/app/homepage.dart
+++ lib/src/app/homepage.dart
@@ -8,20 +8,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items:[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Home"),
+    return DefaultTabController(
+        length: 3,
+        child:Scaffold(
+          body: TabBarView(
+            children: <Widget>[
+              Scaffold(
+                appBar: AppBar(backgroundColor: Colors.white10,),
+              ),
+              Container(color: Colors.orange,),
+              Container(
+                color: Colors.lightGreen,
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Home"),
-            
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.rss_feed),
+              ),
+              Tab(
+                icon: Icon(Icons.perm_identity),
+              ),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Color(0xffe46b10),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: BoxDecoration(
+              borderRadius: BorderRadius.circular(70),
+              gradient: LinearGradient(
+                colors: [Color(0xfffbb448),Color(0xffe46b10)],
+              ),
+            ),
           ),
-        ],
-      ),
-    );
+          backgroundColor: Colors.white10,
+        ),
+      );
   }
 }
